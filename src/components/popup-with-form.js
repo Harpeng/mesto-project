383d2af14c85a4c0b890c7497c86d0255cbee61d
Nastasia <lisitsinan@gmail.com>
2022-07-25 01:22:16 +0300

    Fix - отступы в новых файлах согласно стандарту

--- src/components/popup-with-form.js
+++ src/components/popup-with-form.js
@@ -2,10 +2,10 @@ import { Popup } from "./popup.js";
 
 export class PopupWithForm extends Popup {
   constructor(popupSelector, handleSubmit) {
-      super(popupSelector);
-      this._formElement = this._popup.querySelector('.popup__form');
-      this._inputList = this._formElement.querySelectorAll(".popup__input");
-      this._handleSubmit = handleSubmit;
+    super(popupSelector);
+    this._formElement = this._popup.querySelector('.popup__form');
+    this._inputList = this._formElement.querySelectorAll(".popup__input");
+    this._handleSubmit = handleSubmit;
   }
 
   closePopup() {
--- src/components/section.js
+++ src/components/section.js
@@ -1,21 +1,21 @@
 export default class Section{
-    constructor(containerSelector, {items, renderer}){
-        this._containerSelector = containerSelector;
-        this._items = items;
-        this._renderer = renderer;
-    }
+	constructor(containerSelector, {items, renderer}){
+		this._containerSelector = containerSelector;
+		this._items = items;
+		this._renderer = renderer;
+	}
 
-    renderItem = () => {
-        this._renderer(this._items);
-    }
+	renderItem = () => {
+		this._renderer(this._items);
+	}
 
-    renderItems = () => {
-        this._items.reverse().forEach((item) => {
-        this._renderer(item);
-        })
-    }
+	renderItems = () => {
+		this._items.reverse().forEach((item) => {
+		this._renderer(item);
+		})
+	}
 
-    addItem = (newCard) => {
-        this._containerSelector.prepend(newCard)
-    }
+	addItem = (newCard) => {
+		this._containerSelector.prepend(newCard)
+	}
 }
