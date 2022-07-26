f13760eb8dd2d50703e7391131d3e10ecd1ff378
Nastasia <lisitsinan@gmail.com>
2022-07-26 18:48:39 +0300

    fix: передаем в Section селектор, а не элемент

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -24,39 +24,10 @@ const enableValidationConfig = {
   inactiveButtonClass: 'popup__button_invalid',
   inputErrorClass: 'popup_error-input',
   errorClass: 'popup__error_visible'
-}; 
+};
 
 
 /** карточки */
-const cardContainer = document.querySelector('.elements__container');
-/**const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-  ]; */
+const cardContainer = '.elements__container';
 
-
-
-  
-  export{ avatarSelector,  inputChangeAvatar,  formChangeAvatar, profileChangeAvatarButton, enableValidationConfig, profileAddButton, profileEditButton, formElementEdit, formElementAdd, inputPlace, inputSource, cardContainer, nameInput, jobInput, userNameSelector, userDescSelector};
+export{ avatarSelector,  inputChangeAvatar,  formChangeAvatar, profileChangeAvatarButton, enableValidationConfig, profileAddButton, profileEditButton, formElementEdit, formElementAdd, inputPlace, inputSource, cardContainer, nameInput, jobInput, userNameSelector, userDescSelector};
